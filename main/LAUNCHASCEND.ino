bda79110a1d70b463634ae6fa3c14c4578c4a97e
Collin <craym87@gmail.com>
2022-10-17 21:13:18 -0700

    restart

---
+++ main/LAUNCHASCEND.ino
@@ -0,0 +1,156 @@
+
+
+////------------------------------------------TO-DO-------------------------------
+//GPS SDCARD 9DOF TEMP
+//
+//
+//
+
+
+#include <Adafruit_BNO055.h>
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
+
+/////////////////////////////////////////////////constants//////////////////////////////////////////
+#define RXpin 4
+#define TXpin 3
+
+/* Set the delay between fresh samples */
+uint16_t BNO055_SAMPLERATE_DELAY_MS = 100;
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////Objects///////////////////////////////////////////////////
+//Create instance Software Serial
+SoftwareSerial mySerial(RXpin, TXpin);
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);//Instantiate BNO-055
+
+void setup() {
+  //INITIALIZE BNO 055 ACCELEROMETER
+  if (!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while (1);
+  }
+  delay(1000);
+  // Define pin modes for TX and RX
+  pinMode(RXpin, INPUT);
+
+  pinMode(TXpin, OUTPUT);
+
+  Serial.begin(115200);
+  Serial.println("fuck");
+  mySerial.begin(9600);
+
+
+}
+
+void loop() {
+
+
+
+  Serial.println(mySerial.available());
+  Serial.println("available?^");
+  if (mySerial.available())                           // look for data from GPS module
+  {
+
+    char c = mySerial.read();                           // read in all available chars
+    mySerial.print(c);
+  } else {
+    mySerial.print("GPS FAILURE");
+  }
+  //could add VECTOR_ACCELEROMETER, VECTOR_MAGNETOMETER,VECTOR_GRAVITY...
+  sensors_event_t orientationData , angVelocityData , linearAccelData, magnetometerData, accelerometerData, gravityData;
+  bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
+  bno.getEvent(&angVelocityData, Adafruit_BNO055::VECTOR_GYROSCOPE);
+  bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
+  
+  bno.getEvent(&accelerometerData, Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  bno.getEvent(&gravityData, Adafruit_BNO055::VECTOR_GRAVITY);
+
+  printEvent(&orientationData);
+  printEvent(&angVelocityData);
+  printEvent(&linearAccelData);
+  
+  printEvent(&accelerometerData);
+  printEvent(&gravityData);
+
+  int8_t boardTemp = bno.getTemp();
+  Serial.println();
+  Serial.print(F("temperature: "));
+  Serial.println(boardTemp);
+
+  uint8_t system, gyro, accel, mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
+  Serial.println();
+  Serial.print("Calibration: Sys=");
+  Serial.print(system);
+  Serial.print(" Gyro=");
+  Serial.print(gyro);
+  Serial.print(" Accel=");
+  Serial.print(accel);
+
+
+  Serial.println("--");
+  delay(BNO055_SAMPLERATE_DELAY_MS);
+
+
+}
+
+
+//////////////////////////////////////////Functions///////////////////////////////////
+
+void printEvent(sensors_event_t* event) {
+  double x = -1000000, y = -1000000 , z = -1000000; //dumb values, easy to spot problem
+  if (event->type == SENSOR_TYPE_ACCELEROMETER) {
+    Serial.print("Accl:");
+    x = event->acceleration.x;
+    y = event->acceleration.y;
+    z = event->acceleration.z;
+  }
+  else if (event->type == SENSOR_TYPE_ORIENTATION) {
+    Serial.print("Orient:");
+    x = event->orientation.x;
+    y = event->orientation.y;
+    z = event->orientation.z;
+  }
+  else if (event->type == SENSOR_TYPE_MAGNETIC_FIELD) {
+    Serial.print("Mag:");
+    x = event->magnetic.x;
+    y = event->magnetic.y;
+    z = event->magnetic.z;
+  }
+  else if (event->type == SENSOR_TYPE_GYROSCOPE) {
+    Serial.print("Gyro:");
+    x = event->gyro.x;
+    y = event->gyro.y;
+    z = event->gyro.z;
+  }
+  else if (event->type == SENSOR_TYPE_ROTATION_VECTOR) {
+    Serial.print("Rot:");
+    x = event->gyro.x;
+    y = event->gyro.y;
+    z = event->gyro.z;
+  }
+  else if (event->type == SENSOR_TYPE_LINEAR_ACCELERATION) {
+    Serial.print("Linear:");
+    x = event->acceleration.x;
+    y = event->acceleration.y;
+    z = event->acceleration.z;
+  }
+  else if (event->type == SENSOR_TYPE_GRAVITY) {
+    Serial.print("Gravity:");
+    x = event->acceleration.x;
+    y = event->acceleration.y;
+    z = event->acceleration.z;
+  }
+  else {
+    Serial.print("Unk:");
+  }
+
+  Serial.print("\tx= ");
+  Serial.print(x);
+  Serial.print(" |\ty= ");
+  Serial.print(y);
+  Serial.print(" |\tz= ");
+  Serial.println(z);
+}
