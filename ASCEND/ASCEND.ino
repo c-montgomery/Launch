b0fa1932c28eaca245fc1cfeea23bce5402f1ebb
Collin <craym87@gmail.com>
2023-02-24 15:23:03 -0700

    initial commit

---
+++ ASCEND/ASCEND.ino
@@ -0,0 +1,44 @@
+//----Current working on
+//Functionality check -- 2/9
+//
+//
+//
+//////////////----------Sensors-----------/////////////////////////////////////////////////////////
+//Button
+//GPS
+//Temperature
+//GUVA-S12 UV Sensor
+//UV sensor/LTR390-UV
+//GEIGER COUNTER
+//Altitude/humidity/BMP280
+//MICS2714 breakout
+//MICS-4514
+/////////////////////////////////////////////////
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
